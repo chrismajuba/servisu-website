5bab45d367b7f6fb57fcb3b1cb70118ab9e9e102
chrismajuba <sibusisochris8@gmail.com>
2025-03-28 10:34:24 +0200

    scaffold

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        backgroundColor: "black",
+      }}
+    >
+      <img src="/images/weservelogo.jpg" alt="weservelogo" />
+    </div>
+  );
+};
+
+export default Header;
