653233915689f8cc264705cf309da2045e6f5ab8
chrismajuba <sibusisochris8@gmail.com>
2025-05-28 11:55:39 +0200

    Updated the home page.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,18 +2,15 @@ import React from "react";
 import "./Home.css";
 import Header from "../../modules/core/components/header/Header";
 import Explore from "../../modules/core/components/explore/Explore";
-import { useState } from "react";
 import AppDownload from "../../modules/core/components/app_download/AppDownload";
 import Information from "../../modules/core/components/information/Information";
 
 const Home = () => {
-  const [occupation, setOccupation] = useState(-1);
-
   return (
     <div>
       <Header />
       <Information />
-      <Explore category={occupation} setCategory={setOccupation} />
+      <Explore />
       <AppDownload />
     </div>
   );
